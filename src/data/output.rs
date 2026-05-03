80e989d4b1c1817444e7a9ddcff9b17d0cb51da3
Ysn <yasinnbeyhann@gmail.com>
2026-05-03 14:20:45 +0200

    implement output layer

---
+++ src/data/output.rs
@@ -0,0 +1,24 @@
+use crate::{tensor::tensor::Tensor, util::vec_randomizer::random_vec};
+
+pub struct Output {
+  w: Tensor
+}
+
+impl Output {
+  pub fn new(vocab_size: usize, embedding_dim: usize) -> Self {
+    Output {
+      w: Tensor::new(random_vec(vocab_size * embedding_dim), vec![embedding_dim, vocab_size])
+    }
+  }
+
+  pub fn compute(&self, input: &Tensor) -> Tensor {
+    input.matmul(&self.w)
+      .softmax()
+  }
+}
+
+impl std::fmt::Display for Output {
+  fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+    write!(f, "Output(w={:?})", self.w)
+  }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
+use crate::data::output::Output;
 use crate::tensor::tensor::Tensor;
 use crate::training::attention::Attention;
 use crate::training::embedder::Embedder;
 use crate::data::tokenizer::text_to_indices;
-use crate::training::feed_forward::{self, FeedForward};
+use crate::training::feed_forward::FeedForward;
 
 mod data;
 mod tensor;
@@ -12,12 +13,16 @@ mod util;
 fn main() {
   let indices = text_to_indices("hello");
   let embedder = Embedder::new(26, 4);
-  let forwarded: Tensor = embedder.forward(indices);
+  let embed_forwarded: Tensor = embedder.forward(indices);
 
   let attention: Attention = Attention::new(4);
-  let output = attention.forward(&forwarded);
+  let attention_forward = attention.forward(&embed_forwarded);
 
   let feed_forward = FeedForward::new(4);
-  let activated = feed_forward.forward(&output);
-  println!("{}", activated);
+  let activated = feed_forward.forward(&attention_forward);
+
+  let output = Output::new(26, 4);
+  println!("{}", output);
+  let result = output.compute(&activated);
+  println!("{}", result);
 }
